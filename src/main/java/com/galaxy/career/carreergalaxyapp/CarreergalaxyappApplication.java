99786a4e6aa5cf78f120b03aa59b48662e65a152
ajazahmed12345 <ahmed786ajaz@gmail.com>
2026-02-14 22:26:18 +0530

    Intital Commit for tha App

---
+++ src/main/java/com/galaxy/career/carreergalaxyapp/CarreergalaxyappApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.career.carreergalaxyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarreergalaxyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarreergalaxyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galaxy/career/carreergalaxyapp/CarreergalaxyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.career.carreergalaxyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarreergalaxyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
